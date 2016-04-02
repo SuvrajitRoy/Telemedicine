7af28c3c2f5252772c342b96092f3fa07fde0e27
Roaim <rah0024hr@gmail.com>
2016-04-03 01:51:31 +0600

    Copy database

---
+++ app/src/main/java/com/cste/nstu/suvro/telemedicine/Doctor.java
@@ -0,0 +1,13 @@
+package com.cste.nstu.suvro.telemedicine;
+
+/**
+ * Created by Roaim on 31-Mar-16.
+ */
+public class Doctor {
+    public String id,name;
+
+    public Doctor(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
